09fd39ba9c4a92241ccaf16fa7fb364cad1c17ca
Alima Utkelbayeva <alima150201@gmail.com>
2023-02-25 15:50:34 +0600

    Добавлен метод редактирования и удаления поста

--- webapp/views/guest.py
+++ webapp/views/guest.py
@@ -13,4 +13,30 @@ def add_guest(request: WSGIRequest):
         'text': request.POST.get('text')
     }
     guest = Guest.objects.create(**guest_data)
-    return redirect(f'/', context = guest)
+    return redirect(f'/', pk = guest.pk)
+
+
+def update_view(request,pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'guest_update.html', context={'guest': guest})
+    if request.method == 'POST':
+        guest.user = request.POST.get('user')
+        guest.email = request.POST.get('email')
+        guest.text = request.POST.get('text')
+        guest.save()
+        return redirect('/', pk=pk)
+    return render(request, 'guest_update.html', context={'guest': guest})
+
+
+def delete_view(request, pk):
+
+    guest = get_object_or_404(Guest, pk=pk)
+
+    if request.method == 'GET':
+
+       return render(request, 'guest_delete.html', context={'guest': guest})
+
+    elif request.method == 'POST':
+        guest.delete()
+        return redirect('index')
