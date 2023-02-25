16fe045d268de3092f5b2581fc7d729fb5c37c3a
Alima Utkelbayeva <alima150201@gmail.com>
2023-02-25 18:12:25 +0600

    Слегко изменена модель

--- webapp/models.py
+++ webapp/models.py
@@ -4,6 +4,8 @@ from django.db.models import TextChoices
 class StatusChoice(TextChoices):
     ACTIVE = 'ACTIVE', 'Active'
     NOT_ACTIVE = 'NOT_ACTIVE', 'Not active'
+
+
 class Guest(models.Model):
     user = models.CharField(max_length=100, null=False, blank=False, verbose_name='User name')
     email = models.EmailField(max_length=150, null=False, blank=False, verbose_name= 'Email')
@@ -11,3 +13,6 @@ class Guest(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     update_date = models.DateTimeField(auto_now=True, verbose_name='Updated at')
     status = models.CharField(max_length=100, null=False, blank=False, default=StatusChoice.ACTIVE, verbose_name='Status')
+
+    def __str__(self):
+        return f'{self.user} - {self.email}'
