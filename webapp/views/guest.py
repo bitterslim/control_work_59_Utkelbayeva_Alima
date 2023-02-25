a6dddd126968d27f96af3a96b6ca343c1e1469fe
Alima Utkelbayeva <alima150201@gmail.com>
2023-02-25 18:06:48 +0600

    Добавлены формы

--- webapp/views/guest.py
+++ webapp/views/guest.py
@@ -1,32 +1,38 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, get_object_or_404, redirect
 
+from webapp.forms import GuestForm
 from webapp.models import Guest
 
 
 def add_guest(request: WSGIRequest):
     if request.method == 'GET':
-        return render(request, 'guest_add.html')
-    guest_data = {
-        'user': request.POST.get('user'),
-        'email': request.POST.get('email'),
-        'text': request.POST.get('text')
-    }
-    guest = Guest.objects.create(**guest_data)
-    return redirect(f'/', pk = guest.pk)
+        form = GuestForm()
+        return  render(request, 'guest_add.html', context={'form' : form})
+
+    form = GuestForm(data=request.POST)
+    if not form.is_valid():
+        return  render(request, 'guest_add.html', context={'form' : form})
+    else:
+        guest = Guest.objects.create(**form.cleaned_data)
+        return redirect(f'/', pk=guest.pk)
+
+
+
 
 
 def update_view(request,pk):
     guest = get_object_or_404(Guest, pk=pk)
-    if request.method == 'GET':
-        return render(request, 'guest_update.html', context={'guest': guest})
+
     if request.method == 'POST':
-        guest.user = request.POST.get('user')
-        guest.email = request.POST.get('email')
-        guest.text = request.POST.get('text')
-        guest.save()
-        return redirect('/', pk=pk)
-    return render(request, 'guest_update.html', context={'guest': guest})
+        form = GuestForm(request.POST, instance=guest)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        return render(request, 'guest_update.html', context={'form': form, 'guest': guest})
+
+    form = GuestForm(instance=guest)
+    return render(request, 'guest_update.html', context={'form': form, 'guest': guest})
 
 
 def delete_view(request, pk):
